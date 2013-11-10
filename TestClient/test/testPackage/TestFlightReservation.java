3cf3ccc1160752ebfc983ec16268f4d4adc2d976
jesper1610 <jesperbitsch1610@gmail.com>
2013-11-10 17:24:58 +0100

    Cancel flight

--- TestClient/test/testPackage/TestFlightReservation.java
+++ TestClient/test/testPackage/TestFlightReservation.java
@@ -5,10 +5,17 @@
 package testPackage;
 
 import dk.dtu.imm.airlinereservation.BookFlightFaultMessage;
+import dk.dtu.imm.airlinereservation.CancelFlightFaultMessage;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import dk.dtu.imm.fastmoney.types.CreditCardInfoType;
 import dk.dtu.imm.fastmoney.types.ExpirationDateType;
+import java.util.GregorianCalendar;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
 import org.junit.Before;
 /**
  *
@@ -28,7 +35,20 @@ public class TestFlightReservation {
        creditcard.setNumber("50408816");
        creditcard.setExpirationDate(ex);
     }
-    
+    @Test
+    public void testGetflights() {
+        DatatypeFactory df;
+        try {
+            df = DatatypeFactory.newInstance();
+        } catch (DatatypeConfigurationException ex) {
+            return;
+        }
+        XMLGregorianCalendar date = df.newXMLGregorianCalendar("2013-12-25");
+        String start = "Moscow";
+        String end = "Berlin";
+        int num = getFlights(start,end,date).size();
+        assertEquals(1,num);
+    }   
     @Test
     public void testBookAirlineReservation() {
         try {
@@ -44,5 +64,15 @@ public class TestFlightReservation {
         dk.dtu.imm.airlinereservation.AirlineReservationService service = new dk.dtu.imm.airlinereservation.AirlineReservationService();
         dk.dtu.imm.airlinereservation.AirlineReservationPortType port = service.getAirlineReservationPortTypeBindingPort();
         return port.bookFlight(bookingNumber, creditcard);
+    }
+    private static java.util.List<dk.dtu.imm.airlinereservation.types.FlightInformationType> getFlights(java.lang.String start, java.lang.String end, javax.xml.datatype.XMLGregorianCalendar date) {
+        dk.dtu.imm.airlinereservation.AirlineReservationService service = new dk.dtu.imm.airlinereservation.AirlineReservationService();
+        dk.dtu.imm.airlinereservation.AirlineReservationPortType port = service.getAirlineReservationPortTypeBindingPort();
+        return port.getFlights(start, end, date);
+    }
+    private static boolean cancelFlight(int bookingNumber, dk.dtu.imm.fastmoney.types.CreditCardInfoType creditcard) throws CancelFlightFaultMessage {
+        dk.dtu.imm.airlinereservation.AirlineReservationService service = new dk.dtu.imm.airlinereservation.AirlineReservationService();
+        dk.dtu.imm.airlinereservation.AirlineReservationPortType port = service.getAirlineReservationPortTypeBindingPort();
+        return port.cancelFlight(bookingNumber, creditcard);
     }
 }
