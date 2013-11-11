22f78ed7dda78d9c40a77dca231be88005e5d0f1
jesper1610 <jesperbitsch1610@gmail.com>
2013-11-11 09:01:49 +0100

    changed name

--- TestClient/build/generated/jax-wsCache/AirlineReservationService/dk/dtu/imm/airlinereservation/types/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "urn://types.airlineReservation.imm.dtu.dk", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package dk.dtu.imm.airlinereservation.types;
