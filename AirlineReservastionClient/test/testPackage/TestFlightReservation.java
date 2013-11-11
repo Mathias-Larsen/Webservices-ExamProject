3225ed8ac5d68b1c58bb30b87933b97c7a83312e
jesper1610 <jesperbitsch1610@gmail.com>
2013-11-11 10:16:27 +0100

    Lidt oprydning

--- AirlineReservastionClient/test/testPackage/TestFlightReservation.java
+++ AirlineReservastionClient/test/testPackage/TestFlightReservation.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package testPackage;
 
 import dk.dtu.imm.airlinereservation.BookFlightFaultMessage;
